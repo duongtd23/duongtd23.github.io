1d2e8bbe6962d849447f4c0fd3c6bc1878bcf4e0
duongtd23 <15135951+duongtd23@users.noreply.github.com>
2025-02-25 10:56:33 +0000

    Deploying to gh-pages from @ duongtd23/duongtd23.github.io@a4efa50512fb8a3af2841ec723312db0799370e6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "Projects",
-          description: "A growing collection of your cool projects.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
@@ -46,51 +46,41 @@ ninja.data = [{
           section: "News",},{id: "news-i-moved-to-tomita-and-aoki-s-research-group-working-on-the-formal-methods-and-verification-tools-for-next-generation-automotive-system-platforms-research-project",
           title: 'I moved to Tomita and Aoki’s research group, working on the Formal Methods...',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-enhancing-decision-making-safety-in-ad-through-online-model-checking",
+          title: 'Enhancing Decision-making Safety in AD Through Online Model Checking',
+          description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+              window.location.href = "/projects/AD-safety-shield-through-onlineMC/";
+            },},{id: "projects-a-runtime-verification-framework-for-adss",
+          title: 'A Runtime Verification Framework for ADSs',
+          description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+              window.location.href = "/projects/ADS-runtime-verification/";
+            },},{id: "projects-autoware-safety-analysis-in-cutin-cutout-and-deceleration-scenarios",
+          title: 'Autoware Safety Analysis in cutin, cutout, and deceleration scenarios',
+          description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+              window.location.href = "/projects/Autoware-cutin-cutout-deceleration/";
+            },},{id: "projects-hybrid-post-quantum-tl-formal-analysis-in-maude-npa",
+          title: 'Hybrid Post-Quantum TL formal analysis in Maude-NPA',
+          description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+              window.location.href = "/projects/hybrid-pqtsl-verification/";
+            },},{id: "projects-ipsg-invariant-proof-score-generator",
+          title: 'IPSG - Invariant Proof Score Generator',
+          description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
+              window.location.href = "/projects/invariant-proof-score-generator/";
+            },},{id: "projects-formal-verification-of-mutual-exclusion-protocols",
+          title: 'Formal verification of mutual exclusion protocols',
+          description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
+              window.location.href = "/projects/mutual-exclusion-verification/";
+            },},{id: "projects-formal-analysis-of-post-quantum-hybrid-key-exchange-ssh-transport-layer-protocol",
+          title: 'Formal analysis of Post-Quantum Hybrid Key Exchange SSH Transport Layer Protocol',
+          description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/projects/pqssh-analysis/";
             },},{
         id: 'social-email',
         title: 'email',
